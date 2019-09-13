0e4a658526bad499e5f5b99b6f965cbabec1debb
knifehand <mhurt@msudenver.edu>
2019-09-13 17:59:27 -0600

    validate

--- CarParts/app/models/product.rb
+++ CarParts/app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
 end
+#checks to see if each of the named fields is present and that its contents aren't empty.
+validates :title, :description, :image_url, presence: true
