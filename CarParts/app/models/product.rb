cdf6a8495e9205708ac5195e49619ac52d0c29be
knifehand <mhurt@msudenver.edu>
2019-09-25 09:01:00 -0600

    update travis yml

--- CarParts/app/models/product.rb
+++ CarParts/app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
 end
-#checks to see if each of the named fields is present and that its contents aren't empty.
+#checks to see if each of the named fields are present and that its contents aren't empty.
 validates :title, :description, :image_url, presence: true
 #checks to verify if price is valid
 validates :price, numericality: {greater_than_or_equal_to: 0.01}
